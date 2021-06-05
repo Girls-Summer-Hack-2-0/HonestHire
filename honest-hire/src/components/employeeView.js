acd4e51ccbf1f81cf9a93dae08dcae352d0f56f0
Lavínia Paganini <laviniapaganini@gmail.com>
2021-06-05 15:48:10 -0300

    Git rebase

--- honest-hire/src/components/employeeView.js
+++ honest-hire/src/components/employeeView.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { auth } from '../firebase';
 import Menu from './Menu';
-// import employeeNav from './Navbar/Navbar';
 
 const employeeView = () => {
     return (
