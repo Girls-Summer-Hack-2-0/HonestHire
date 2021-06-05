4d33082b3aa4c3d827530678900e1561eaa42ff8
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-06-05 16:19:39 +0800

    Add components

---
+++ honest-hire/src/components/employeeSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const SignUp = () => {
+    return (
+        <div className = "container">
+            <div className = "top-container">
+                <h1>employeeSignUp</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;
