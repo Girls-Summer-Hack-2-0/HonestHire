7e1cb393d5b2ff370ec9698b4127268d9bc26111
Lavínia Paganini <laviniapaganini@gmail.com>
2021-06-05 18:17:15 -0300

    Add Firestore query example

--- honest-hire/src/components/Home.js
+++ honest-hire/src/components/Home.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import { auth } from '../firebase';
-import Menu from './Menu';
+import React, { useState, useEffect } from "react";
+import db, { auth } from "../firebase";
+import Menu from "./Menu";
 
 const Home = () => {
-    return (
-        <div>
-            <Menu />
-            <h1>Hello ! welcome to Blind Hiring web :D</h1>
-            <p><button onClick={() => auth.signOut()}>signOut</button></p>
-        </div>
-    )
+  const [companyName, setName] = useState("");
+
+  useEffect(() => {
+    async function getInformation() {
+      console.log(auth.currentUser);
+      const email = auth.currentUser?.email;
+      console.log(email);
+      await db
+        .collection("recruiters")
+        .where("email", "==", email)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            setName(doc.data().company);
+          });
+        })
+        .catch((e) => console.log(e));
+    }
+    getInformation();
+  }, []);
+  return (
+    <div>
+      <Menu />
+      <br></br>
+      <h1>Hello ! welcome to Blind Hiring web :D</h1>
+      <p>{companyName}</p>
+      <p>
+        <button onClick={() => auth.signOut()}>signOut</button>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
