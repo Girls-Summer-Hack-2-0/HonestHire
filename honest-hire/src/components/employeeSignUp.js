881db2b6ed0027f9b953d421bde190447ace6e5a
Jiyoung Yoon <56244911+ebbuni1023@users.noreply.github.com>
2021-06-05 05:13:11 -0500

    loginpage update

--- honest-hire/src/components/employeeSignUp.js
+++ honest-hire/src/components/employeeSignUp.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
+import './employeeSignUp.css';
+
 const SignUp = () => {
+    const history = useHistory()
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    // SIGN UP 
+    const signUp = e =>{
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value).then(user =>{
+                console.log(user)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    // SIGN IN
+    const signIn = e =>{
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+            ).then(user =>{
+                console.log(user)
+                history.push("/employeeView")
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <div className = "container">
-            <div className = "top-container">
-                <h1>employeeSignUp</h1>
-
-            </div>
+            <form action="">
+                <h1>Employee Sign in</h1>
+                <input ref={emailRef}type="email" />
+                <input ref={passwordRef}type="password"/>
+                <button onClick={signIn}>Sign in</button>
+                <h6>Not yet registerd? <span onClick={signUp}className="signin_link">Sign up</span></h6>
+            </form>
         </div>
     )
 }
---
+++ honest-hire/src/components/employerView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { auth } from '../firebase';
+
+const employerView = () => {
+    return (
+        <div>
+            <h1>Hello ! employer~~~~ :D</h1>
+            {/* <p><button onClick={() => auth.signOut()}>signOut</button></p> */}
+        </div>
+    )
+};
+
+export default employerView;
