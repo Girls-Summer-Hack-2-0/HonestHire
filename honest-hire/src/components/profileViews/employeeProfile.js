056ab691ef8b6d9fb312cda99206af2b6ba42fe9
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-06-06 15:40:52 +0800

    Add employee and employer profile pages

---
+++ honest-hire/src/components/profileViews/employeeProfile.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import db, { auth } from "../../firebase";
+
+const EmployeeProfile = () => {
+    const [position, setPosition] = useState("");
+    const [skills, setSkills] = useState("");
+    const [education, setEducation] = useState("");
+    const [graduation, setgraduation] = useState("");
+    const [experience, setExperience] = useState("");
+    const [projects, setProjects] = useState("");
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        async function getInformation() {
+          console.log(auth.currentUser);
+          const email = auth.currentUser?.email;
+          console.log(email);
+          await db
+            .collection("users")
+            .where("email", "==", email)
+            .get()
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+
+                setPosition(doc.data().Position);
+                setSkills(doc.data().Skills);
+                setEducation(doc.data().education);
+                setgraduation(doc.data().graduation);
+                setExperience(doc.data().Experience);
+                setProjects(doc.data().projects);
+                setUsername(doc.data().username);
+
+              });
+            })
+            .catch((e) => console.log(e));
+        }
+        getInformation();
+      }, []);
+
+    return (
+        
+        <div className = "container">
+            <br></br>
+            <h1>Hello ! welcome to Blind Hiring web :D</h1>
+            <p>Position: {position}</p>
+            <p>Education: {education}</p>
+            <p>Graduation: {graduation}</p>
+            <p>Experience: {experience}</p>
+            <p>Projects: {projects}</p>
+            <p>Username: {username}</p>
+            <p>Skills: {skills}</p>
+
+            <p>
+                <button onClick={() => auth.signOut()}>signOut</button>
+            </p>
+
+        </div>
+    )
+}
+
+
+export default EmployeeProfile;
--- honest-hire/src/pages/Employer/Home/index.js
+++ honest-hire/src/pages/Employer/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { auth } from '../../../firebase';
 import Menu from '../../../components/Menu';
+import EmployerProfile from '../../../components/profileViews/employerProfile';
 
 const employerView = () => {
     return (
         <div>
             <Menu isEmployer={true} />
+            <EmployerProfile/>
+
             <br></br>
             <h1>Hello ! employer~~~~ :D</h1>
             {/* <p><button onClick={() => auth.signOut()}>signOut</button></p> */}
