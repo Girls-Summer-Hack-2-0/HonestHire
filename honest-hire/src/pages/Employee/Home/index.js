6f1fc31f5cc4472580f0c90ef6289aebfab143c3
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-06-06 15:40:52 +0800

    Add employee and employer profile pages

--- honest-hire/src/pages/Employee/Home/index.js
+++ honest-hire/src/pages/Employee/Home/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { auth } from '../../../firebase';
 import Menu from '../../../components/Menu';
+import EmployeeProfile from '../../../components/profileViews/employeeProfile';
 
 const employeeView = () => {
     return (
         
         <div>
             <Menu isInterviewee={true}/>
+            <EmployeeProfile/>
 
             <h1>Hello ! employee~~~~ :D</h1>
             {/* <p><button onClick={() => auth.signOut()}>signOut</button></p> */}
+
+
         </div>
     )
 };
